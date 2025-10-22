c9675a1d504ec414ebb4ef3497169aac7026811a
Chris Nizzardini <cnizzardini@gmail.com>
2025-10-22 08:27:04 -0400

    wip

--- tests/TestCase/Response/ResponseModifierTest.php
+++ tests/TestCase/Response/ResponseModifierTest.php
@@ -12,10 +12,8 @@ class ResponseModifierTest extends TestCase
 {
     public function test_event_listener(): void
     {
-        $modifier = new ResponseModifier('jsonld', 'MixerApi/JsonLdView.JsonLd');
-        $modifier->listen();
-        $eventManager = EventManager::instance();
-        $listeners = $eventManager->matchingListeners('Controller.initialize');
+        (new ResponseModifier('jsonld', 'MixerApi/JsonLdView.JsonLd'))->listen();
+        $listeners = EventManager::instance()->matchingListeners('Controller.initialize');
         $names = [];
         foreach ($listeners['Controller.initialize'] as $listens) {
             foreach ($listens as $listen) {
